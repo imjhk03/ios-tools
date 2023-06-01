7a1891c52b937607b7b959ef8dc861a7dd40e9f7
Joohee Kim <imjhk03@gmail.com>
2023-06-01 16:07:14 +0900

    Add Supplementary views related functions

--- Sources/Reusable/UICollectionView+Reusable.swift
+++ Sources/Reusable/UICollectionView+Reusable.swift
@@ -21,12 +21,6 @@ public extension UICollectionView {
         register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
     
-    func registerCells<T: UICollectionViewCell>(_ cellTypes: [T.Type]) where T: Reusable {
-        for cellType in cellTypes {
-            registerCell(cellType.self)
-        }
-    }
-    
     func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T where T: Reusable {
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
@@ -34,5 +28,39 @@ public extension UICollectionView {
 
         return cell
     }
+    
+    enum SupplementaryViewKind {
+        case header
+        case footer
+        
+        var rawValue: String {
+            switch self {
+            case .header:
+                return UICollectionView.elementKindSectionHeader
+            case .footer:
+                return UICollectionView.elementKindSectionFooter
+            }
+        }
+    }
+    
+    func registerView<T: UICollectionReusableView>(_ viewClass: T.Type, forSupplementaryViewKind: SupplementaryViewKind) where T: Reusable {
+        register(T.self, forSupplementaryViewOfKind: forSupplementaryViewKind.rawValue, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func registerView<T: UICollectionReusableView>(_ viewClass: T.Type, forSupplementaryViewKind: SupplementaryViewKind) where T: NibReusable {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        
+        register(nib, forSupplementaryViewOfKind: forSupplementaryViewKind.rawValue, withReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableSupplementaryView<T: UICollectionReusableView>(ofKind elementKind: SupplementaryViewKind, for indexPath: IndexPath) -> T where T: Reusable {
+        guard let view = dequeueReusableSupplementaryView(ofKind: elementKind.rawValue, withReuseIdentifier: T.reuseIdentifier, for: indexPath)  as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        
+        return view
+    }
+    
 }
 #endif
--- Sources/Reusable/UITableView+Reusable.swift
+++ Sources/Reusable/UITableView+Reusable.swift
@@ -21,18 +21,31 @@ public extension UITableView {
         register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
     }
     
-    func registerCells<T: UITableViewCell>(_ cellTypes: [T.Type]) where T: Reusable {
-        for cellType in cellTypes {
-            registerCell(cellType.self)
-        }
-    }
-    
     func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T where T: Reusable {
         guard let cell = dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
 
         return cell
+    }
+    
+    func registerView<T: UITableViewHeaderFooterView>(_ viewClass: T.Type) where T: Reusable {
+        register(T.self, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func registerView<T: UITableViewHeaderFooterView>(_ viewClass: T.Type) where T: NibReusable {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        
+        register(nib, forHeaderFooterViewReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(_ viewClass: T.Type) -> T where T: Reusable {
+        guard let view = dequeueReusableHeaderFooterView(withIdentifier: T.reuseIdentifier) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        
+        return view
     }
 }
 #endif
